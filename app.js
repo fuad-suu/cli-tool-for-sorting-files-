df9a2afacedabb1ad11fed3fc45bbe245f6ba994
fuad <demonicfuad@gmail.com>
2026-09-24 18:58:54 +0300

    simple cli tool done

---
+++ app.js
@@ -0,0 +1,74 @@
+import fs from "node:fs";
+import path from "node:path";
+
+const files = fs.readdirSync("downloads");
+console.log(files.length);
+
+for (const file of files) {
+  const extension = path.extname(file);
+  const mainPath = "downloads";
+
+  switch (extension) {
+    case ".pdf":
+      {
+        const subfolder = path.join(mainPath, "PDF");
+        fs.mkdirSync(subfolder, { recursive: true });
+        const source = path.join("downloads", file);
+        const destination = path.join("downloads", "PDF", file);
+        fs.renameSync(source, destination);
+      }
+      break;
+
+    case ".jpg":
+      {
+        const subfolder = path.join(mainPath, "JPG");
+        fs.mkdirSync(subfolder, { recursive: true });
+        const source = path.join("downloads", file);
+        const destination = path.join("downloads", "JPG", file);
+        fs.renameSync(source, destination);
+      }
+      break;
+
+    case ".png":
+      {
+        const subfolder = path.join(mainPath, "PNG");
+        fs.mkdirSync(subfolder, { recursive: true });
+        const source = path.join("downloads", file);
+        const destination = path.join("downloads", "PNG", file);
+        fs.renameSync(source, destination);
+      }
+      break;
+
+    case ".mp3":
+      {
+        const subfolder = path.join(mainPath, "MP3");
+        fs.mkdirSync(subfolder, { recursive: true });
+        const source = path.join("downloads", file);
+        const destination = path.join("downloads", "MP3", file);
+        fs.renameSync(source, destination);
+      }
+      break;
+
+    case ".mp4":
+      {
+        const subfolder = path.join(mainPath, "MP4");
+        fs.mkdirSync(subfolder, { recursive: true });
+        const source = path.join("downloads", file);
+        const destination = path.join("downloads", "MP4", file);
+        fs.renameSync(source, destination);
+      }
+      break;
+
+    case ".txt":
+      {
+        const subfolder = path.join(mainPath, "TEXT");
+        fs.mkdirSync(subfolder, { recursive: true });
+        const source = path.join("downloads", file);
+        const destination = path.join("downloads", "TEXT", file);
+        fs.renameSync(source, destination);
+      }
+      break;
+  }
+}
+const mainPath = "downloads";
+console.log(path.join(mainPath, "test"));
